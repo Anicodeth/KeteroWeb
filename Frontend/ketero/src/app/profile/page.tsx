de65509ede96d4258e895a014753013cdb3598c6
yohanse <mehabawyohanse793@gmail.com>
2024-02-05 12:27:35 +0300

    fix ui and add toggle

--- Frontend/ketero/src/app/profile/page.tsx
+++ Frontend/ketero/src/app/profile/page.tsx
@@ -1,5 +1,5 @@
-import Customers from '@/components/Business/Customers'
-import React from 'react'
+import Customers from '@/components/Business/Customers';
+import React from 'react';
 
 const page = () => {
   return (
