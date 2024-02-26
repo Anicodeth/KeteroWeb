96bd8f7a0e649fbab264997c65c233fc26ad9eab
Ananya <afmtrendaid@gmail.com>
2024-02-26 17:21:43 +0300

    change

---
+++ Frontend/ketero/src/services/AuthService.tsx
@@ -0,0 +1,18 @@
+
+
+
+export  async function signUpClient(){
+
+}
+
+export  async function signUpBusiness(){
+
+}
+
+export  async function signUpMezgeb(){
+
+}
+
+export  async function signIn(){
+
+}
