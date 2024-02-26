a7a6c7954d0448a2b23b8a8e1c46b9b7c3f9ed75
Ananya <afmtrendaid@gmail.com>
2024-02-26 17:53:06 +0300

    change

--- Frontend/ketero/src/services/AuthService.tsx
+++ Frontend/ketero/src/services/AuthService.tsx
@@ -1,12 +1,18 @@
 import { Client } from "@/models/Client";
 import axios from "axios";
+import { captureRejectionSymbol } from "events";
 
 
 const url = "";
 
 export  async function signUpClient(client: Client): Promise<any>{
+    try{
     const response = axios.post(`${url}` + "/api/api/signup", client).then((res:any) => res.data); 
-    return response.data;
+    return response.data;}
+    catch(e){
+        console.log(e);
+        throw e;
+    }
 }
 
 export  async function signUpBusiness(){
