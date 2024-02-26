163913b339ea34ee332312077ae4015dd4108ec7
Ananya <afmtrendaid@gmail.com>
2024-02-26 22:51:18 +0300

    change

--- Frontend/ketero/src/services/AuthService.tsx
+++ Frontend/ketero/src/services/AuthService.tsx
@@ -1,56 +1,47 @@
+import axios from "axios";
 import { Business } from "@/models/Business";
 import { Client } from "@/models/Client";
 import { Login } from "@/models/Login";
 import { Mezgeb } from "@/models/Mezgeb";
-import axios from "axios";
 
+const apiUrl = "/api"; // Assuming this is your base API URL
 
-const url = "";
-
-export  async function signUpClient(client: Client): Promise<any>{
-    try{
-    const response = await axios.post(`${url}` + "/api/api/signup", client).then((res:any) => res.data); 
-    return response.data;}
-    catch(e){
-        console.log(e);
+export async function signUpClient(client: Client): Promise<any> {
+    try {
+        const response = await axios.post(`${apiUrl}/signup/client`, client);
+        return response.data;
+    } catch (e) {
+        console.error("Error signing up client:", e);
         throw e;
     }
 }
 
-export  async function signUpBusiness(business: Business):Promise<any>{
-
-    try{
-        const response = await axios.post(`${url}`+ "/api/api/signup", business).then(
-            (res:any)=> res.data);
-
+export async function signUpBusiness(business: Business): Promise<any> {
+    try {
+        const response = await axios.post(`${apiUrl}/signup/business`, business);
         return response.data;
-    }
-    catch(e){
+    } catch (e) {
+        console.error("Error signing up business:", e);
         throw e;
     }
 }
 
-export  async function signUpMezgeb(mezgeb: Mezgeb){
-
-    try{
-        const response = await axios.post("", mezgeb).then(
-            (res:any)=>res.data
-        );
+export async function signUpMezgeb(mezgeb: Mezgeb): Promise<any> {
+    try {
+        const response = await axios.post(`${apiUrl}/signup/mezgeb`, mezgeb);
         return response.data;
+    } catch (e) {
+        console.error("Error signing up mezgeb:", e);
+        throw e;
     }
-    catch(e){
-        throw e
+}
+
+export async function signIn(login: Login): Promise<any> {
+    try {
+        const response = await axios.post(`${apiUrl}/signin`, login);
+        return response.data;
+    } catch (e) {
+        console.error("Error signing in:", e);
+        throw e;
     }
-
 }
-
-export  async function signIn(login:Login){
-try{
-    const response = await axios.post("", login).then((res:any)=>res.data);
-    return response.data;
-}
-catch(e){
-    throw e
-}
-}
-
