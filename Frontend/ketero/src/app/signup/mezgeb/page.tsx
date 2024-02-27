dc497c71324ded399c2179e1e20c535072d58b05
Ananya <afmtrendaid@gmail.com>
2024-02-27 15:28:13 +0300

    change

--- Frontend/ketero/src/app/signup/mezgeb/page.tsx
+++ Frontend/ketero/src/app/signup/mezgeb/page.tsx
@@ -1,23 +1,85 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
+import { useMutation, QueryClient, QueryClientProvider } from "react-query";
 import { CiUser } from "react-icons/ci";
 import { FaGoogle, FaFacebookSquare } from "react-icons/fa";
 import { AiOutlineApple } from "react-icons/ai";
+import { z, ZodError } from "zod";
 
-import styles from "./Mezgeb.module.css";
+import styles from "./Client.module.css";
 import Link from "next/link";
+import { signUpClient } from "../../../services/AuthService";
+import { Client } from "@/models/Client";
+import { toast } from "sonner";
+
+const queryClient = new QueryClient();
 
 const SignUp: React.FC = () => {
   return (
-    <div className={[styles.screen, , styles.center].join(" ")}>
-      <div className={[styles.mainWrapper].join(" ")}>
+    <QueryClientProvider client={queryClient}>
+      <Form />
+    </QueryClientProvider>
+  );
+};
+
+const Form: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const signupSchema = z.object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const signupMutation = useMutation(
+    (newClient: Client) => signUpClient(newClient),
+    {
+      onSuccess: () => {
+        toast("Signup successful");
+      },
+      onError: (error: any) => {
+        toast(error.response.data.error);
+        console.error("Error signing up:", error.response.data.error);
+      },
+    }
+  );
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        return "Password not matching";
+      }
+      signupSchema.parse({ name, email, password });
+      await signupMutation.mutateAsync({
+        name,
+        email,
+        password,
+      });
+    } catch (error: any) {
+      if (error instanceof ZodError) {
+        console.error("Validation error:", error.errors);
+      } else {
+        console.error("Error signing up:", error.response.data);
+      }
+    }
+  };
+
+  return (
+    <div className={[styles.screen, styles.center].join(" ")}>
+      <div className={styles.mainWrapper}>
         <h1></h1>
         <div>
-          <h1 className={[styles.heading].join(" ")}>Welcome</h1>
-          <p className={[styles.paragraph].join(" ")}>
+          <h1 className={styles.heading}>Welcome</h1>
+          <p className={styles.paragraph}>
             Hi, Enter your details to get sign up to your account
           </p>
         </div>
-        <form action="" className={[styles.form].join(" ")}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <div>
             <div className={[styles.inputWrapper, styles.center].join(" ")}>
               <div className={[styles.center].join(" ")}>
@@ -25,25 +87,15 @@ const SignUp: React.FC = () => {
               </div>
               <input
                 type="text"
-                placeholder="Company's Name"
+                placeholder="Name"
                 className="outline-none border-none"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <hr />
           </div>
-          <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
-              </div>
-              <input
-                type="text"
-                placeholder="Owner's Name"
-                className="outline-none border-none"
-              />
-            </div>
-            <hr />
-          </div>
+
           <div>
             <div className={[styles.inputWrapper, styles.center].join(" ")}>
               <div className={[styles.center].join(" ")}>
@@ -53,6 +105,8 @@ const SignUp: React.FC = () => {
                 type="text"
                 placeholder="Email"
                 className="outline-none border-none"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <hr />
@@ -66,6 +120,8 @@ const SignUp: React.FC = () => {
                 type="text"
                 placeholder="Password"
                 className="outline-none border-none"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <hr />
@@ -79,28 +135,46 @@ const SignUp: React.FC = () => {
                 type="text"
                 placeholder="Confirm Password"
                 className="outline-none border-none"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <hr />
           </div>
-          <button className={[styles.signupButton].join(" ")}>Signup</button>
-          <p className={styles.login}>Did you have an account already ? <Link href="/signin"><span>Login</span></Link></p>
+          <button
+            type="submit"
+            className={styles.signupButton}
+            disabled={signupMutation.isLoading}
+          >
+            {signupMutation.isLoading ? "Signing up..." : "Signup"}
+          </button>
+          <p className={styles.login}>
+            Did you have an account already ?{" "}
+            <Link href="/signin">
+              <span>Login</span>
+            </Link>
+          </p>
         </form>
+
         <div>
-          <p className={[styles.paragraph].join(" ")}>Or Sign in via</p>
-          <div className={[styles.otherOption].join(" ")}>
+          <p className={styles.paragraph}>Or Sign in via</p>
+          <div className={styles.otherOption}>
             <div className={[styles.containerIcon, styles.center].join(" ")}>
               <FaGoogle
-                className={[styles.icon].join(" ")}
+                className={styles.icon}
                 size={20}
                 color="#FF56A5"
               />
             </div>
             <div className={[styles.containerIcon, styles.center].join(" ")}>
-              <AiOutlineApple className={[styles.icon].join(" ")} size={20} color="grey"/>
+              <AiOutlineApple className={styles.icon} size={20} color="grey" />
             </div>
             <div className={[styles.containerIcon, styles.center].join(" ")}>
-              <FaFacebookSquare className={[styles.icon].join(" ")} size={20} color="#197DCA"/>
+              <FaFacebookSquare
+                className={styles.icon}
+                size={20}
+                color="#197DCA"
+              />
             </div>
           </div>
         </div>
