cc28461291de3e707770bdc52b9f9ebd195ab32a
Ananya <afmtrendaid@gmail.com>
2024-02-27 19:50:47 +0300

    change

--- Frontend/ketero/src/app/page.tsx
+++ Frontend/ketero/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import Singin from './signin/page'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(()=>{
+    router.push("/signup")
+  }, [])
   return (
-    <div>
-      <Singin />
-    </div>
+    <h1>Ketero</h1>
   )
 }
