df513f1f60d8e9d0030bf57469f87469c8893b64
Ananya <afmtrendaid@gmail.com>
2024-02-28 11:18:48 +0300

    change

---
+++ Frontend/ketero/src/app/mezgebu/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React, { useState } from 'react';  // Import useState from React
+import style from '@/app/mezgebu/Mezgebu.module.css';
+import { FaHome, FaPlus, FaUser } from 'react-icons/fa';
+
+import BusinessProfile from '@/components/business/BusinessProfile';
+import Customers from '@/components/business/Customers';
+// import AddService from '@/components/business/AddService';
+
+const Mezgebu: React.FC = () => {
+  const [toggle, setToggle] = useState<string>('Customer');
+
+  const handleClick = (activity: string) => {
+    setToggle(activity);
+  };
+
+  return (
+    <div className={style.activityContainer}>
+      <div className={style.activeWindow}>
+         {toggle === 'Customer' ? <Customers /> : ''} 
+         {toggle === 'Profile' ? <BusinessProfile /> : ''} 
+         {/* {toggle === 'AddService' ? <AddService /> : ''}  */}
+      </div>
+
+      <div className={style.bottomNav}>
+        <button
+          className={style.bottomNavButton}
+          onClick={() => handleClick('Customer')}
+        >
+          <FaHome className={style.buttonIcon} />
+          <p className={style.buttonText}>Home</p>
+        </button>
+
+        <button
+          className={style.bottomNavButton}
+          onClick={() => handleClick('AddService')}
+        >
+          <FaPlus className={style.buttonIcon} />
+          <p className={style.buttonText}>Add</p>
+        </button>
+
+        <button
+          className={style.bottomNavButton}
+          onClick={() => handleClick('Profile')}
+        >
+          <FaUser className={style.buttonIcon} />
+          <p className={style.buttonText}>Profile</p>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Mezgebu;
