61e11cef5eda91cc2f95871bc0b29df397c55049
Ananya <afmtrendaid@gmail.com>
2024-02-28 11:31:50 +0300

    changes

--- Frontend/ketero/src/services/ServiceServices.ts
+++ Frontend/ketero/src/services/ServiceServices.ts
@@ -2,15 +2,31 @@ import axios from "axios";
 
 const apiUrl = ""
 
-export async function getService(){
 
+export async function getService(){
+    try{
+
+    }
+    catch(e){
+        
+    }
 }
 
 export async function getServices(){
+    try{
 
-
+    }
+    catch(e){
+        
+    }
 }
 
 export async function createServices(){
-    
+
+    try{
+
+    }
+    catch(e){
+        
+    }
 }
