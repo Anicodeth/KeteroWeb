a5ba1b7c1c6efea46be1858ba08d64d477042b8a
Ananya <afmtrendaid@gmail.com>
2024-02-28 15:39:35 +0300

    changes

--- Backend/controllers/serviceController.js
+++ Backend/controllers/serviceController.js
@@ -1,4 +1,18 @@
 const { getService, getServices, updateService, deleteService, createService } = require('../services/serviceService');
+const admin = require('firebase-admin');
+const { v4: uuidv4 } = require('uuid');
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+// Get a Firestore instance
+const db = admin.firestore();
+
+// Get a Storage instance
+const storage = admin.storage().bucket();
+
 
 exports.getService = async (req, res) => {
     try {
@@ -22,14 +36,43 @@ exports.getServices = async (req, res) => {
 exports.createService = async (req, res) => {
     try {
         const { name, description, price } = req.body;
+        const image = req.file; // Assuming req.file contains the uploaded file
 
-        const serviceData = { name, description, price};
-        const service = await createService(serviceData);
-        res.status(201).json(service);
+        // Generate a unique filename for the image
+        const filename = `${uuidv4()}_${image.originalname}`;
+
+        // Upload the image to Firebase Storage
+        const fileUpload = storage.file(filename);
+        await fileUpload.save(image.buffer, {
+            metadata: {
+                contentType: image.mimetype
+            }
+        });
+
+        // Get the download URL of the uploaded image
+        const imageUrl = await fileUpload.getSignedUrl({
+            action: 'read',
+            expires: '01-01-2200'
+        });
+
+        // Create a new service document in Firestore with the image download URL
+        const docRef = await db.collection('services').add({
+            name: name,
+            description: description,
+            price: price,
+            image: imageUrl[0] // Get the first URL from the array
+        });
+
+        // Get the newly created service document
+        const serviceSnapshot = await docRef.get();
+        const serviceData = serviceSnapshot.data();
+
+        res.status(201).json(serviceData);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
+
 
 exports.updateService = async (req, res) => {
     try {
