3f8e1ba7a928e9bde08355f067e61de9e5bc7758
Ananya <afmtrendaid@gmail.com>
2024-02-29 15:14:42 +0300

    change

--- Frontend/ketero/src/models/Service.ts
+++ Frontend/ketero/src/models/Service.ts
@@ -5,11 +5,12 @@ interface CreateService {
     name: string;
     description: string; 
     price:string;
+    image:File;
 
 }
 
 interface Service {
-    
+
     _id:string;
     name: string;
     description: string; 
