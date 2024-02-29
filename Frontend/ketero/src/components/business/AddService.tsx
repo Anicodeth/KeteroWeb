b578e7b231b38a1dac8d3579638f7c89116628b2
Ananya <afmtrendaid@gmail.com>
2024-02-29 11:49:53 +0300

    change

--- Frontend/ketero/src/components/business/AddService.tsx
+++ Frontend/ketero/src/components/business/AddService.tsx
@@ -1,88 +1,106 @@
 import React, { useState } from "react";
 import style from "./AddService.module.css";
 import { IoIosAddCircle } from "react-icons/io";
+import { toast } from "sonner";
+import { motion } from "framer-motion"
+import { z, ZodError } from "zod";
+import { useMutation, QueryClient, QueryClientProvider } from "react-query";
+
 
 const AddService: React.FC = () => {
-  const [image, setImage] = useState<File | null>(null);
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [payment, setPayment] = useState<string>("");
+    const queryClient = new QueryClient();
 
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setImage(e.target.files[0]);
-    }
-  };
-
-  const handleSubmit = ()=>{
-    
-  }
 
   return (
-
-    <div className={[style.wrapper].join(" ")}>
-      <div className={[style.inputImage].join(" ")}>
-        {image ? (
-          <img src={URL.createObjectURL(image)} alt="" />
-        ) : (
-          <IoIosAddCircle size={60} color="#700F14" />
-        )}
-        <label htmlFor="image" className={[style.icon].join(" ")}>
-          +
-          <input
-            type="file"
-            id="image"
-            accept="image/*"
-            style={{ display: "none" }}
-            onChange={handleImageChange}
-          />
-        </label>
-      </div>
-      <div className={[style.inputFields].join(" ")}>
-        <div>
-          <label htmlFor="serviceName" className={[style.block].join(" ")}>
-            Service Title
-          </label>
-          <input
-            type="text"
-            id="serviceName"
-            value = {name}
-            onChange = {(e) => setName(e.target.value)}
-            className={[style.input].join(" ")}
-          />
-        </div>
-        <div>
-          <label htmlFor="payment" className={[style.block].join(" ")}>
-            Payment
-          </label>
-          <input
-            type="text"
-            id="payment"
-            className={[style.input].join(" ")}
-            value = {payment}
-            onChange = {(e) => setPayment(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="description" className={[style.block].join(" ")}>
-            Description
-          </label>
-          <input
-            type="text"
-            id="description"
-            className={[style.input].join(" ")}
-            value = {description}
-            onChange = {(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <button onClick={()=>handleSubmit} className={[style.buttonAddService].join(" ")}>
-          Add Service
-        </button>
-
-      </div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Form />
+    </QueryClientProvider>
   );
 };
 
+
+function Form() {
+    const [image, setImage] = useState<File | null>(null);
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [payment, setPayment] = useState<string>("");
+  
+  
+  
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length > 0) {
+        setImage(e.target.files[0]);
+      }
+    };
+  
+    const handleSubmit = ()=>{
+  
+    }
+    return (
+        <>
+        <div className={[style.wrapper].join(" ")}>
+        <div className={[style.inputImage].join(" ")}>
+          {image ? (
+            <img src={URL.createObjectURL(image)} alt="" />
+          ) : (
+            <IoIosAddCircle size={60} color="#700F14" />
+          )}
+          <label htmlFor="image" className={[style.icon].join(" ")}>
+            +
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={handleImageChange}
+            />
+          </label>
+        </div>
+        <div className={[style.inputFields].join(" ")}>
+          <div>
+            <label htmlFor="serviceName" className={[style.block].join(" ")}>
+              Service Title
+            </label>
+            <input
+              type="text"
+              id="serviceName"
+              value = {name}
+              onChange = {(e) => setName(e.target.value)}
+              className={[style.input].join(" ")}
+            />
+          </div>
+          <div>
+            <label htmlFor="payment" className={[style.block].join(" ")}>
+              Payment
+            </label>
+            <input
+              type="text"
+              id="payment"
+              className={[style.input].join(" ")}
+              value = {payment}
+              onChange = {(e) => setPayment(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="description" className={[style.block].join(" ")}>
+              Description
+            </label>
+            <input
+              type="text"
+              id="description"
+              className={[style.input].join(" ")}
+              value = {description}
+              onChange = {(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <button onClick={()=>handleSubmit} className={[style.buttonAddService].join(" ")}>
+            Add Service
+          </button>
+  
+        </div>
+      </div>
+      </>
+    )
+
+}
 export default AddService;
