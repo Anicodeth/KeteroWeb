e81ed0ddd210096913ba55454332d60dc9bd601e
Ananya <afmtrendaid@gmail.com>
2024-03-01 14:57:09 +0300

    change

---
+++ Frontend/ketero/src/app/client/layout.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function clientLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+  }
