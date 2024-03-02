2542d7dd7f36474dc3b07bc8df2a70134b82bd2b
Ananya <afmtrendaid@gmail.com>
2024-03-02 15:44:37 +0300

    change

--- Frontend/ketero/src/app/client/reservation/[serviceId]/page.tsx
+++ Frontend/ketero/src/app/client/reservation/[serviceId]/page.tsx
@@ -1,26 +1,23 @@
-"use client"
 
+"use client"
 import { useState } from 'react';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
-import { getService } from "../../../../services/ServiceServices";
-import { useQuery, useMutation } from "react-query";
-import { useParams } from '../../../../../node_modules/next/navigation';
+import { getService } from '../../../../services/ServiceServices';
+import { useQuery, useMutation } from 'react-query';
+import { useParams } from 'next/navigation';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/card';
 import { Service } from '@/models/Service';
-import { Grid } from "react-loader-spinner";
-import {createReservation} from "../../../../services/ReservationService";
-import { Reservation } from '@/models/Reservation';
-import {toast} from 'sonner';
-import { Skeleton } from "@/components/ui/skeleton"
+import { Grid } from 'react-loader-spinner';
+import { createReservation } from '../../../../services/ReservationService';
+import { toast } from 'sonner';
 
- 
 const ReservationSchema = z.object({
   date: z.string(),
   time: z.string(),
@@ -31,20 +28,21 @@ type ReservationData = z.infer<typeof ReservationSchema>;
 const ReservationForm = () => {
   const [formData, setFormData] = useState<Partial<ReservationData>>({});
   const [errors, setErrors] = useState<z.ZodIssue[] | null>(null);
-  const [dateAndTime, setDateAndTime ] = useState<any>();
+  const [dateAndTime, setDateAndTime] = useState<any>();
 
-  const [businessId, setBusinessId] = useState("");
-  const [clientId, setClientId] = useState("")
+  const [businessId, setBusinessId] = useState('');
+  const [clientId, setClientId] = useState('');
 
+  const params = useParams();
+  const serviceId: string = params.serviceId as string;
 
-   const params = useParams()
-   const serviceId:string = params.serviceId as string;
+  const { data, isLoading, isError } = useQuery('service', () => getService(serviceId));
+  const createReservationMutation = useMutation((reservation: Reservation) => createReservation(reservation), {
+    onSuccess: () => {
+      toast('Reservation created successfully');
+    },
+  });
 
-
-  const {data, isLoading, isError} = useQuery( "service", () => getService(serviceId))
-  const createReservationMutation = useMutation((reservation:Reservation) => createReservation(reservation));
-
-  
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
@@ -58,52 +56,39 @@ const ReservationForm = () => {
     if (result.success) {
       const combined = new Date(`${formData.date}T${formData.time}`);
       setDateAndTime(combined);
-      
+
       setBusinessId(data.businessId);
-    
-      const sessionData:any = JSON.parse(sessionStorage.getItem("user")!);
+
+      const sessionData: any = JSON.parse(sessionStorage.getItem('user')!);
       setClientId(sessionData._id);
-      const client = sessionData._id;
 
-      if ( !client || !businessId || !dateAndTime || !serviceId){
-        toast("Missing Data")
-        return 
+      if (!clientId || !businessId || !dateAndTime || !serviceId) {
+        toast('Missing Data');
+        return;
       }
-      createReservationMutation.mutateAsync(
-        {
-          clientId: client,
-          businessId,
-          dateAndTime,
-          serviceId
-        }
-      );
-
-
-      
+      createReservationMutation.mutateAsync({
+        clientId,
+        businessId: data.businessId,
+        dateAndTime,
+        serviceId,
+      });
 
       setFormData({});
       setErrors(null);
     } else {
-      console.log(result)
       setErrors(result.error.errors);
     }
   };
 
   return (
-
-    <form onSubmit={handleSubmit} className="max-w-md flex flex-col p-10  mx-auto">
-      {isLoading &&<div className = "flex items-center justify-center"> <Grid
-        visible={true}
-        height="80"
-        width="80"
-        color="#700F14"
-        ariaLabel="grid-loading"
-        radius="12.5"
-        wrapperStyle={{}}
-        wrapperClass="grid-wrapper"
-        /></div>}
+    <form onSubmit={handleSubmit} className="max-w-md flex flex-col p-10  mx-auto" disabled={createReservationMutation.isLoading}>
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          <Grid visible={true} height="80" width="80" color="#700F14" ariaLabel="grid-loading" radius="12.5" wrapperStyle={{}} wrapperClass="grid-wrapper" />
+        </div>
+      )}
       {isError && <div> Error Fetching </div>}
-      {data && <ServiceShadCard service={data}></ServiceShadCard>}
+      {data && <ServiceShadCard service={data} />}
 
       <label htmlFor="date">Date:</label>
       <input
@@ -114,10 +99,7 @@ const ReservationForm = () => {
         onChange={handleChange}
         className="border border-gray-300 rounded-md px-3 py-2 mt-1 mb-3"
       />
-      {errors && errors.find(error => error.path[0] === 'date') && (
-        <div className="text-red-500">Date is required</div>
-      )}
-
+      {errors && errors.find((error) => error.path[0] === 'date') && <div className="text-red-500">Date is required</div>}
 
       <label htmlFor="time">Time:</label>
       <input
@@ -128,33 +110,22 @@ const ReservationForm = () => {
         onChange={handleChange}
         className="border border-gray-300 rounded-md px-3 py-2 mt-1 mb-3"
       />
-      {errors && errors.find(error => error.path[0] === 'time') && (
-        <div className="text-red-500">Time is required</div>
-      )}
-        <Button>{createReservationMutation.isLoading ? "Reserving..." : "Submit Reservation"}</Button>
+      {errors && errors.find((error) => error.path[0] === 'time') && <div className="text-red-500">Time is required</div>}
+      <Button type="submit">{createReservationMutation.isLoading ? 'Reserving...' : 'Submit Reservation'}</Button>
     </form>
-
   );
-
-
-
-  
 };
-
 
 const ServiceShadCard: React.FC<{ service: Service }> = ({ service }) => {
   return (
-      <Card className="h-fit">
-        <CardHeader>
-          <div
-            className="inset-0 bg-cover h-40 bg-center"
-            style={{ backgroundImage: `url(${service.imageUrl})` }}
-          ></div>
-          <CardTitle>{service.name}</CardTitle>
-          <CardDescription>{service.description}</CardDescription>
-          <CardDescription>{service.price}</CardDescription>
-        </CardHeader>
-      </Card>
+    <Card className="h-fit">
+      <CardHeader>
+        <div className="inset-0 bg-cover h-40 bg-center" style={{ backgroundImage: `url(${service.imageUrl})` }}></div>
+        <CardTitle>{service.name}</CardTitle>
+        <CardDescription>{service.description}</CardDescription>
+        <CardDescription>{service.price}</CardDescription>
+      </CardHeader>
+    </Card>
   );
 };
 
