398be0ae18a6da95ea0c43c9fd4cf96087b2204f
Ananya <afmtrendaid@gmail.com>
2024-03-06 22:37:03 +0300

    changes

--- Frontend/ketero/src/components/business/BusinessProfile.tsx
+++ Frontend/ketero/src/components/business/BusinessProfile.tsx
@@ -1,8 +1,8 @@
 import style from "./BusinessProfile.module.css";
 import { FaMoneyBillTransfer, FaBookmark } from "react-icons/fa6";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import {useQuery} from 'react-query'
-import { Skeleton } from "@/components/ui/skeleton"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { useQuery } from "react-query";
+import { Skeleton } from "@/components/ui/skeleton";
 import { getPendingData } from "@/services/ReservationService";
 import { getBusiness } from "@/services/BusinessService";
 
@@ -11,12 +11,15 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import { motion } from "framer-motion";
 import { getService } from "@/services/ServiceServices";
 
 const BusinessProfile: React.FC = () => {
-  const user = typeof window !== 'undefined' ? JSON.parse(sessionStorage.getItem('user')!) : null;
+  const user =
+    typeof window !== "undefined"
+      ? JSON.parse(sessionStorage.getItem("user")!)
+      : null;
 
   const { data: business } = useQuery("business", () => getBusiness(user._id));
 
@@ -24,10 +27,9 @@ const BusinessProfile: React.FC = () => {
   const confirmed = business?.confirmed;
   const services = business?.services;
 
-  if(!business){
-    return null
+  if (!business) {
+    return null;
   }
-  
 
   return (
     <div className={style.clientContainer}>
@@ -54,13 +56,12 @@ const BusinessProfile: React.FC = () => {
           <h5>Pending Services</h5>
           <h4>{pending && pending.length}</h4>
         </div>
-
       </div>
       <div className={style.hiredContainer}>
         <h2>Services Confirmed</h2>
         {confirmed && confirmed.length > 0 ? (
           <div className={style.hiredServices}>
-            {confirmed.map((reservationId:string, index:any) => (
+            {confirmed.map((reservationId: string, index: any) => (
               <HiredServiceCard key={index} reservationId={reservationId} />
             ))}
           </div>
@@ -70,71 +71,79 @@ const BusinessProfile: React.FC = () => {
         <h2>Services Pending</h2>
         {pending && pending.length > 0 ? (
           <div className={style.hiredServices}>
-            {pending.map((reservationId:string, index:any) => (
-              <HiredServiceCard key={reservationId} reservationId={reservationId} />
+            {pending.map((reservationId: string, index: any) => (
+              <HiredServiceCard
+                key={reservationId}
+                reservationId={reservationId}
+              />
             ))}
           </div>
         ) : (
           <p>No pending services</p>
         )}
 
-      <h2>Services Offered</h2>
-      {services && services.length > 0 ? (
-        <div className={style.hiredServices}>
-          {services.map((serviceId:string, index:any) => (
-                        <motion.div
-                        initial = {{ opacity: 0, x:-50}}
-                        animate = {{ opacity: 1, x: 0}}
-                        transition= {{ duration: 0.5, delay:0.1}}
-                        >
-              <ServiceShadCard key={serviceId} serviceId={serviceId} />
-            </motion.div>
-          ))}
-        </div>
-      ) : (
-        <p>No services offered</p>
-      )}
+        <h2>Services Offered</h2>
+        {services && services.length > 0 ? (
+          <div className={style.hiredServices}>
+            {services.map((serviceId: string, index: any) => (
+              <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: 0.1 }}
+              >
+                <ServiceShadCard key={serviceId} serviceId={serviceId} />
+              </motion.div>
+            ))}
+          </div>
+        ) : (
+          <p>No services offered</p>
+        )}
       </div>
-
     </div>
   );
 };
-const HiredServiceCard: React.FC<{ reservationId: string }> = ({ reservationId }) => {
-
-  const { data: reservationData, isLoading: isReservationLoading, isError: isReservationError } = useQuery(["reservation", reservationId], () => getPendingData(reservationId));
+const HiredServiceCard: React.FC<{ reservationId: string }> = ({
+  reservationId,
+}) => {
+  const {
+    data: reservationData,
+    isLoading: isReservationLoading,
+    isError: isReservationError,
+  } = useQuery(["reservation", reservationId], () =>
+    getPendingData(reservationId)
+  );
 
   if (isReservationLoading) {
-    return <SkeletonCard />
-    ;
+    return <SkeletonCard />;
   }
 
-  if (isReservationError ) {
+  if (isReservationError) {
     return null;
   }
 
-
   return (
     <motion.div
-    initial = {{ opacity: 0, x:-50}}
-    animate = {{ opacity: 1, x: 0}}
-    transition= {{ duration: 0.5, delay:0.1}}
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
     >
-    <div className={style.cardContainer}>
-      <div className = "flex flex-row items-center"> 
-      <img  className = "bg-cover h-8 w-10" src = {reservationData.imageUrl}></img>
-      <div className={style.serviceData}>
-        <h3>{reservationData.serviceName}</h3>
-        <h5>{(reservationData.serviceDescription).slice(0, 10) + "..."}</h5>
-      </div>
-      </div>
+      <div className={style.cardContainer}>
+        <div className="flex flex-row items-center">
+          <img
+            className="bg-cover h-8 w-10"
+            src={reservationData.imageUrl}
+          ></img>
+          <div className={style.serviceData}>
+            <h3>{reservationData.serviceName}</h3>
+            <h5>{reservationData.serviceDescription.slice(0, 10) + "..."}</h5>
+          </div>
+        </div>
 
-
-      <h3>{reservationData.servicePrice} ETB</h3>
-    </div>
+        <h3>{reservationData.servicePrice} ETB</h3>
+      </div>
     </motion.div>
   );
 };
-
 
 export function SkeletonCard() {
   return (
@@ -145,12 +154,15 @@ export function SkeletonCard() {
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
-  )
+  );
 }
 
-
 const ServiceShadCard: React.FC<{ serviceId: string }> = ({ serviceId }) => {
-  const { data: service, isLoading, isError } = useQuery(["service", serviceId], () => getService(serviceId));
+  const {
+    data: service,
+    isLoading,
+    isError,
+  } = useQuery(["service", serviceId], () => getService(serviceId));
 
   if (isLoading) {
     return <SkeletonCard />;
@@ -162,18 +174,23 @@ const ServiceShadCard: React.FC<{ serviceId: string }> = ({ serviceId }) => {
 
   return (
     <motion.div
-    initial = {{ opacity: 0, x:-50}}
-    animate = {{ opacity: 1, x: 0}}
-    transition= {{ duration: 0.5, delay:0.1}}
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
     >
-    <Card className="h-fit">
-      <CardHeader>
-       { service.imageUrl && <div className="inset-0 bg-cover h-40 bg-center" style={{ backgroundImage: `url(${service.imageUrl})` }}></div>}
-        <CardTitle>{service.name}</CardTitle>
-        <CardDescription>{service.description}</CardDescription>
-        <CardDescription>{service.price}</CardDescription>
-      </CardHeader>
-    </Card>
+      <Card className="h-fit">
+        <CardHeader>
+          {service.imageUrl && (
+            <div
+              className="inset-0 bg-cover h-40 bg-center"
+              style={{ backgroundImage: `url(${service.imageUrl})` }}
+            ></div>
+          )}
+          <CardTitle>{service.name}</CardTitle>
+          <CardDescription>{service.description}</CardDescription>
+          <CardDescription>{service.price}</CardDescription>
+        </CardHeader>
+      </Card>
     </motion.div>
   );
 };
