bed2a683ce1653b857fe65018e53078f7129e852
Ananya <afmtrendaid@gmail.com>
2024-03-07 11:49:16 +0300

    changes

--- Backend/controllers/MezgebuController.js
+++ Backend/controllers/MezgebuController.js
@@ -1,8 +1,5 @@
 const { getMezgebu } = require("../services/MezgebuService");
 
-
-
-
 exports.getMezgebu = async (req, res) => {
   try {
     const { id } = req.params;
@@ -14,32 +11,31 @@ exports.getMezgebu = async (req, res) => {
 };
 
 exports.getMezgebues = async (req, res) => {
-    try {
-        const mezgebues = await getMezgebues();
-        res.status(200).json(mezgebues);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-    };
+  try {
+    const mezgebues = await getMezgebues();
+    res.status(200).json(mezgebues);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 exports.updateMezgebu = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updateData = req.body;
-        const mezgebu = await updateMezgebu(id, updateData);
-        res.status(200).json(mezgebu);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-    }
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+    const mezgebu = await updateMezgebu(id, updateData);
+    res.status(200).json(mezgebu);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 exports.deleteMezgebu = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const mezgebu = await deleteMezgebu(id);
-        res.status(200).json(mezgebu);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-    }
-
+  try {
+    const { id } = req.params;
+    const mezgebu = await deleteMezgebu(id);
+    res.status(200).json(mezgebu);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
