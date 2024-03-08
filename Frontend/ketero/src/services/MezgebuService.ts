0a854d05e5f04eecf4c828c3e7c82404233a134a
Ananya <afmtrendaid@gmail.com>
2024-03-08 19:51:18 +0300

    changes

--- Frontend/ketero/src/services/MezgebuService.ts
+++ Frontend/ketero/src/services/MezgebuService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const apiUrl = "https://ketero-web-dmow.vercel.app/api/v1/client";
-export async function getClient(id: string): Promise<any> {
+const apiUrl = "https://ketero-web-dmow.vercel.app/api/v1/mezgebu";
+export async function getMezgebu(id: string): Promise<any> {
   try {
     const response = await axios.get(`${apiUrl}/${id}`);
     return response.data;
@@ -9,3 +9,14 @@ export async function getClient(id: string): Promise<any> {
     throw e;
   }
 }
+
+export async function getMezgebus(): Promise<any> {
+  try {
+    const response = await axios.get(`${apiUrl}`);
+    return response.data;
+  } catch (e) {
+    throw e;
+  }
+}
+
+
