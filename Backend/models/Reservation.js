d12e03921122c4e223c4b5cf0f2c5a92cbb44519
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:06:57 +0300

    Changes

--- Backend/models/Reservation.js
+++ Backend/models/Reservation.js
@@ -1,28 +1,26 @@
 const mongoose = require("mongoose");
 
 const ReservationSchema = mongoose.Schema({
-    serviceId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Service'
-    },
-    businessId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Business'
-    },
-    clientId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Client'
-    },
-    dateAndTime: {
-        type: Date, 
-        required: true
-    },
-    confirmed: {
-        type: Boolean,
-        default: false 
-    },
-
+  serviceId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Service",
+  },
+  businessId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Business",
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
+  dateAndTime: {
+    type: Date,
+    required: true,
+  },
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-
-module.exports = mongoose.model('Reservation', ReservationSchema);
+module.exports = mongoose.model("Reservation", ReservationSchema);
