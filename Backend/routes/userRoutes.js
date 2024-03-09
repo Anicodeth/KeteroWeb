71209f1cb158405933dd452f39687f98ba68d91f
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:13:03 +0300

    Changes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController.js');
-const authMiddleware = require('../middlewares/authMiddleware.js');
+const router = require("express").Router();
+const userController = require("../controllers/userController.js");
+const authMiddleware = require("../middlewares/authMiddleware.js");
 
 router.use(authMiddleware);
 
-router.get('/', userController.getUser);
+router.get("/", userController.getUser);
 
 module.exports = router;
