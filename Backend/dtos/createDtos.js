885c0307a47cabc58c215f54d7b9f1a0f8972156
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:11:46 +0300

    Changes

--- Backend/dtos/createDtos.js
+++ Backend/dtos/createDtos.js
@@ -1,29 +1,34 @@
 const createClientDTO = {
-    name: String,
-    email: String,
-    phone:String,
-    password: String
-  };
-  
-  // DTOs for createBusiness
-  const createBusinessDTO = {
-    email: String,
-    ownerName: String,
-    businessName: String,
-    password: String
-  };
-  
-  // DTOs for createMezgeb
-  const createMezgebDTO = {
-    name: String,
-    email: String,
-    password: String
-  };
-  
-  // DTOs for loginUser
-  const loginUserDTO = {
-    email: String,
-    password: String
-  };
-  
-  module.exports = { createClientDTO, createBusinessDTO, createMezgebDTO, loginUserDTO };
+  name: String,
+  email: String,
+  phone: String,
+  password: String,
+};
+
+// DTOs for createBusiness
+const createBusinessDTO = {
+  email: String,
+  ownerName: String,
+  businessName: String,
+  password: String,
+};
+
+// DTOs for createMezgeb
+const createMezgebDTO = {
+  name: String,
+  email: String,
+  password: String,
+};
+
+// DTOs for loginUser
+const loginUserDTO = {
+  email: String,
+  password: String,
+};
+
+module.exports = {
+  createClientDTO,
+  createBusinessDTO,
+  createMezgebDTO,
+  loginUserDTO,
+};
