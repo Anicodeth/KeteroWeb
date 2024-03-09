4da95a358d6d230e0b6f145859fa3dd272d39f6c
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:09:36 +0300

    Changes

--- Backend/services/clientService.js
+++ Backend/services/clientService.js
@@ -1,15 +1,13 @@
-
-const Client = require('../models/Client');
+const Client = require("../models/Client");
 
 exports.getClient = async (id) => {
-    try {
-        const client = await Client.findById(id);
-        if (!client) {
-            throw new Error('Client not found');
-        }
-        return client;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const client = await Client.findById(id);
+    if (!client) {
+      throw new Error("Client not found");
     }
-}
-
+    return client;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
