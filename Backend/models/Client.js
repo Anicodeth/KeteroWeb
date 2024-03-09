d0edec2c3e5fb04e70d63e100f0eca132ee6332f
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:07:09 +0300

    Changes

--- Backend/models/Client.js
+++ Backend/models/Client.js
@@ -1,44 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ClientSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        minLength: [3, "Name must exceed 3 characters"]
-    } ,
-    email:{
-        type: String,
-        required: true,
-        validate:{
-            validator: function (value){
-                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
-            },
-            message: () => 'Not a valid email format'
-        }
+  name: {
+    type: String,
+    required: true,
+    minLength: [3, "Name must exceed 3 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+      },
+      message: () => "Not a valid email format",
     },
-    password: {
-        type: String,
-        required: true,
-        minLength: [8, "Password must be at least 8 Characters"]
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [8, "Password must be at least 8 Characters"],
+  },
+
+  phone: {
+    type: String,
+  },
+  pending: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reservation",
     },
-
-    phone:{
-        type: String,
-    } 
-    ,
-    pending: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref: 'Reservation'
-        }
-    ],
-    confirmed: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref: 'Reservation'
-        }
-    ]
-
+  ],
+  confirmed: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reservation",
+    },
+  ],
 });
 
-module.exports = mongoose.model('Client', ClientSchema);
+module.exports = mongoose.model("Client", ClientSchema);
