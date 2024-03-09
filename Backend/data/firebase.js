4130b511d3aad511b9aaa8440e5546b6882b07e1
Ananya <afmtrendaid@gmail.com>
2024-03-09 21:59:24 +0300

    Prettier Config and Linting

--- Backend/data/firebase.js
+++ Backend/data/firebase.js
@@ -1,11 +1,9 @@
-
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 var serviceAccount = require("../ketero-72e14-79dbd1c309b9.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://ketero-72e14.appspot.com/'
+  storageBucket: "gs://ketero-72e14.appspot.com/",
 });
-
 
 module.exports = admin;
