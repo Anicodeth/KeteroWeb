ea54b9c248dc8157d950ad8d81e2e9488b17e41b
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:10:46 +0300

    Changes

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,9 @@
-const { createClient, createBusiness, createMezgeb, loginUser } = require('../services/authService');
+const {
+  createClient,
+  createBusiness,
+  createMezgeb,
+  loginUser,
+} = require("../services/authService");
 
 // Controller for creating a client
 exports.createClient = async (req, res) => {
@@ -15,92 +20,98 @@ exports.createClient = async (req, res) => {
 exports.createBusiness = async (req, res) => {
   try {
     const { ownerName, businessName, email, password } = req.body;
-    const business = await createBusiness({ ownerName, businessName, email, password });
+    const business = await createBusiness({
+      ownerName,
+      businessName,
+      email,
+      password,
+    });
     res.status(201).json(business);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-};const Business = require('../models/Business');
-const Service = require('../models/Service');
+};
+const Business = require("../models/Business");
+const Service = require("../models/Service");
 
 exports.getBusinessServices = async (businessId) => {
-    try {
-        const business = await Business.findOne(businessId);
-        if (!business) {
-            throw new Error("Invalid Business Id");
-        }
-        return business.services;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const business = await Business.findOne(businessId);
+    if (!business) {
+      throw new Error("Invalid Business Id");
     }
+    return business.services;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 exports.addServiceToBusiness = async (data) => {
-    const { businessId, serviceName, serviceDescription, price } = data;
-    try {
-        const business = await Business.findById(businessId);
-        const service = new Service({ serviceName, serviceDescription, price });
-        if (!business || !service) {
-            throw new Error("Invalid data entry");
-        }
-
-        business.services.push(service);
-        await business.save();
-        return service;
-
-    } catch (error) {
-        throw new Error(error.message);
+  const { businessId, serviceName, serviceDescription, price } = data;
+  try {
+    const business = await Business.findById(businessId);
+    const service = new Service({ serviceName, serviceDescription, price });
+    if (!business || !service) {
+      throw new Error("Invalid data entry");
     }
+
+    business.services.push(service);
+    await business.save();
+    return service;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 exports.getBusiness = async (id) => {
-    try {
-        const business = await Business.findById(id);
-        if (!business) {
-            throw new Error("Invalid Business Id");
-        }
-        return business;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const business = await Business.findById(id);
+    if (!business) {
+      throw new Error("Invalid Business Id");
     }
+    return business;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 exports.getBusinesses = async () => {
-    try {
-        const businesses = await Business.find();
-        if (!businesses.length) {
-            throw new Error("No businesses found");
-        }
-        return businesses;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const businesses = await Business.find();
+    if (!businesses.length) {
+      throw new Error("No businesses found");
     }
+    return businesses;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 exports.updateBusiness = async (id, updateData) => {
-    try {
-        const business = await Business.findByIdAndUpdate(id, updateData, { new: true });
-        if (!business) {
-            throw new Error("Invalid Business Id");
-        }
-        return business;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const business = await Business.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    if (!business) {
+      throw new Error("Invalid Business Id");
     }
+    return business;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 exports.deleteBusiness = async (id) => {
-    try {
-        const business = await Business.findByIdAndDelete(id);
-        if (!business) {
-            throw new Error("Invalid Business Id");
-        }
-        return business;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const business = await Business.findByIdAndDelete(id);
+    if (!business) {
+      throw new Error("Invalid Business Id");
     }
+    return business;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
-
 
 // Controller for creating a mezgeb
 exports.createMezgeb = async (req, res) => {
