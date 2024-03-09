bcd911e25db1f92ce4ce7029a7922135c46fa43d
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:10:59 +0300

    Changes

--- Backend/controllers/clientController.js
+++ Backend/controllers/clientController.js
@@ -1,12 +1,11 @@
-
-const { getClient } = require('../services/clientService');
+const { getClient } = require("../services/clientService");
 
 exports.getClient = async (req, res) => {
-    try {
-        const { clientId } = req.params;
-        const client = await getClient(clientId);
-        res.status(200).json(client);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const { clientId } = req.params;
+    const client = await getClient(clientId);
+    res.status(200).json(client);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
