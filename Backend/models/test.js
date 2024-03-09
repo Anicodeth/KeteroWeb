aafd3c9ec3a530e4088c4ec1e9d777af05f714e1
Ananya <afmtrendaid@gmail.com>
2024-03-09 22:06:43 +0300

    Changes

--- Backend/models/test.js
+++ Backend/models/test.js
@@ -1,6 +1,6 @@
-const Service = require('./Service');
-const { Admin, Business, Mezgeb, Client } = require('./User');
-const mongoose = require('mongoose');
+const Service = require("./Service");
+const { Admin, Business, Mezgeb, Client } = require("./User");
+const mongoose = require("mongoose");
 
 mongoose
   .connect(
@@ -9,21 +9,20 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB", err));
 
-
 const service = new Service({
-  name: 'Service 1',
-  description: 'Description of Service 1',
-  price: 100
+  name: "Service 1",
+  description: "Description of Service 1",
+  price: 100,
 });
 
-service.save()
-  .then(savedService => {
-    console.log('Service saved:', savedService);
+service
+  .save()
+  .then((savedService) => {
+    console.log("Service saved:", savedService);
   })
-  .catch(err => {
-    console.error('Error saving service:', err);
+  .catch((err) => {
+    console.error("Error saving service:", err);
   });
-
 
 // Create and save an admin
 // const admin = new Admin({
@@ -34,43 +33,42 @@ service.save()
 // admin.save();
 
 const client = new Client({
-    email: 'admnin@example.com',
-    password: 'adminpassword',
-    name: 'Client Name',
-  });
-client.save()
-
+  email: "admnin@example.com",
+  password: "adminpassword",
+  name: "Client Name",
+});
+client.save();
 
 // Create and save a business
 const business = new Business({
-  email: 'businessg@example.com',
-  password: 'businesspassword',
-  businessName: 'Business Name',
-  OwnerName: 'Business Owner',
+  email: "businessg@example.com",
+  password: "businesspassword",
+  businessName: "Business Name",
+  OwnerName: "Business Owner",
 });
 
 business.services.push(service);
-business.save()
-  .then(savedBusiness => {
-    console.log('Business saved:', savedBusiness);
+business
+  .save()
+  .then((savedBusiness) => {
+    console.log("Business saved:", savedBusiness);
   })
-  .catch(err => {
-    console.error('Error saving business:', err);
+  .catch((err) => {
+    console.error("Error saving business:", err);
   });
 
 // Create and save a mezgeb
 const mezgeb = new Mezgeb({
-  email: 'mezgegb@example.com',
-  password: 'mezgebpassword',
-  mezgebField: 'Mezgeb Field',
+  email: "mezgegb@example.com",
+  password: "mezgebpassword",
+  mezgebField: "Mezgeb Field",
 });
 
-
-
-mezgeb.save()
-  .then(savedMezgeb => {
-    console.log('Mezgeb saved:', savedMezgeb);
+mezgeb
+  .save()
+  .then((savedMezgeb) => {
+    console.log("Mezgeb saved:", savedMezgeb);
   })
-  .catch(err => {
-    console.error('Error saving mezgeb:', err);
+  .catch((err) => {
+    console.error("Error saving mezgeb:", err);
   });
