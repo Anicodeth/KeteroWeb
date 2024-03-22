3c2e9f4f1c52495cb6a37d7ba914858db922bfed
Ananya <afmtrendaid@gmail.com>
2024-03-22 10:53:26 +0300

    add service

--- Frontend/ketero/src/app/signup/business/page.tsx
+++ Frontend/ketero/src/app/signup/business/page.tsx
@@ -29,12 +29,14 @@ const Form: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [phone, setPhone] = useState("");
 
   const signupSchema = z.object({
     businessName: z.string().min(2),
     ownerName: z.string().min(2),
     email: z.string().email(),
     password: z.string().min(6),
+    phone:z.string()
   });
 
   const signupMutation = useMutation(
@@ -74,136 +76,150 @@ const Form: React.FC = () => {
 
   return (
     <motion.div
-    initial={{ opacity: 0, x: -50 }}
-    animate={{ opacity: 1, x: 0 }}
-    transition={{ duration: 0.5, delay: 0.1 }}>
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
+    >
+      <div className={[styles.screen, styles.center].join(" ")}>
+        <div className={styles.mainWrapper}>
+          <h1></h1>
+          <div>
+            <h1 className={styles.heading}>Welcome</h1>
+            <p className={styles.paragraph}>
+              Hi, Enter your details to get sign up to your account
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Company's Name"
+                  className="outline-none border-none"
+                  value={businessName}
+                  onChange={(e) => setBusinessName(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Owner's Name"
+                  className="outline-none border-none"
+                  value={ownerName}
+                  onChange={(e) => setOwnerName(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="phone"
+                  placeholder="Phone"
+                  className="outline-none border-none"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="outline-none border-none"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Password"
+                  className="outline-none border-none"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
+            <div>
+              <div className={[styles.inputWrapper, styles.center].join(" ")}>
+                <div className={[styles.center].join(" ")}>
+                  <CiUser></CiUser>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Confirm Password"
+                  className="outline-none border-none"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <hr />
+            </div>
 
+            <button
+              type="submit"
+              className={styles.signupButton}
+              disabled={signupMutation.isLoading}
+            >
+              {signupMutation.isLoading ? "Signing up..." : "Signup"}
+            </button>
 
-    <div className={[styles.screen, styles.center].join(" ")}>
-      <div className={styles.mainWrapper}>
-        <h1></h1>
-        <div>
-          <h1 className={styles.heading}>Welcome</h1>
-          <p className={styles.paragraph}>
-            Hi, Enter your details to get sign up to your account
-          </p>
-        </div>
-        <form onSubmit={handleSubmit} className={styles.form}>
+            <p className={styles.login}>
+              Did you have an account already ?{" "}
+              <Link href="/signin">
+                <span>Login</span>
+              </Link>
+            </p>
+          </form>
           <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
+            <p className={styles.paragraph}>Or Sign in via</p>
+            <div className={styles.otherOption}>
+              <div className={[styles.containerIcon, styles.center].join(" ")}>
+                <FaGoogle className={styles.icon} size={20} color="#FF56A5" />
               </div>
-              <input
-                type="text"
-                placeholder="Company's Name"
-                className="outline-none border-none"
-                value={businessName}
-                onChange={(e) => setBusinessName(e.target.value)}
-              />
-            </div>
-            <hr />
-          </div>
-          <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
+              <div className={[styles.containerIcon, styles.center].join(" ")}>
+                <AiOutlineApple
+                  className={styles.icon}
+                  size={20}
+                  color="grey"
+                />
               </div>
-              <input
-                type="text"
-                placeholder="Owner's Name"
-                className="outline-none border-none"
-                value={ownerName}
-                onChange={(e) => setOwnerName(e.target.value)}
-              />
-            </div>
-            <hr />
-          </div>
-          <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
+              <div className={[styles.containerIcon, styles.center].join(" ")}>
+                <FaFacebookSquare
+                  className={styles.icon}
+                  size={20}
+                  color="#197DCA"
+                />
               </div>
-              <input
-                type="text"
-                placeholder="Email"
-                className="outline-none border-none"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <hr />
-          </div>
-          <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
-              </div>
-              <input
-                type="text"
-                placeholder="Password"
-                className="outline-none border-none"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <hr />
-          </div>
-          <div>
-            <div className={[styles.inputWrapper, styles.center].join(" ")}>
-              <div className={[styles.center].join(" ")}>
-                <CiUser></CiUser>
-              </div>
-              <input
-                type="text"
-                placeholder="Confirm Password"
-                className="outline-none border-none"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-            <hr />
-          </div>
-
-          <button
-            type="submit"
-            className={styles.signupButton}
-            disabled={signupMutation.isLoading}
-          >
-            {signupMutation.isLoading ? "Signing up..." : "Signup"}
-          </button>
-      
-          <p className={styles.login}>
-            Did you have an account already ?{" "}
-            <Link href="/signin">
-              <span>Login</span>
-            </Link>
-          </p>
-        </form>
-        <div>
-          <p className={styles.paragraph}>Or Sign in via</p>
-          <div className={styles.otherOption}>
-            <div className={[styles.containerIcon, styles.center].join(" ")}>
-              <FaGoogle
-                className={styles.icon}
-                size={20}
-                color="#FF56A5"
-              />
-            </div>
-            <div className={[styles.containerIcon, styles.center].join(" ")}>
-              <AiOutlineApple className={styles.icon} size={20} color="grey" />
-            </div>
-            <div className={[styles.containerIcon, styles.center].join(" ")}>
-              <FaFacebookSquare
-                className={styles.icon}
-                size={20}
-                color="#197DCA"
-              />
             </div>
           </div>
         </div>
       </div>
-    </div>
     </motion.div>
   );
 };
