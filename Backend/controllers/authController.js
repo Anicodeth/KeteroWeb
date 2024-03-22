42861d957ec410ca070b7dcd42664a331cfe82f4
Ananya <afmtrendaid@gmail.com>
2024-03-22 11:47:09 +0300

    check update

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -127,8 +127,7 @@ exports.createMezgeb = async (req, res) => {
 // Controller for user login
 exports.loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const data = await loginUser({ email, password });
+    const data = await loginUser(req.body);
     res.status(200).json(data);
   } catch (error) {
     res.status(401).json({ error: error.message });
