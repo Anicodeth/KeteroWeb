95b9ddc1c9281d7d5aed4f00a9d40d7550d67405
Ananya <afmtrendaid@gmail.com>
2024-05-10 14:55:45 +0300

    coming soon

---
+++ Frontend/ketero/src/components/ui/comingSoon.tsx
@@ -0,0 +1,8 @@
+
+export default function ComingSoon() {
+    return (
+        <div className="flex items-center justify-center h-full space-x-4">
+            <div className="text-2xl font-semibold text-primary">Coming Soon!</div>
+        </div>
+    )
+    }
